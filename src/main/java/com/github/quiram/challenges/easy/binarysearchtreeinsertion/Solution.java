0adc2963b367950df63682d59eb966b58053e01d
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-04-01 15:52:04 -0700

    solve "Binary Search Tree: Insertion"

---
+++ src/main/java/com/github/quiram/challenges/easy/binarysearchtreeinsertion/Solution.java
@@ -0,0 +1,66 @@
+package com.github.quiram.challenges.easy.binarysearchtreeinsertion;
+
+import java.util.Scanner;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+/**
+ * <a href="https://www.hackerrank.com/challenges/binary-search-tree-insertion/problem">Binary Search Tree: Insertion</a>
+ */
+class Solution {
+
+    public static void preOrder(Node root) {
+
+        if (root == null)
+            return;
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+
+    }
+
+ /* Node is defined as :
+ class Node
+    int data;
+    Node left;
+    Node right;
+
+    */
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        }
+
+        if (data < root.data) {
+            root.left = insert(root.left, data);
+        } else {
+            root.right = insert(root.right, data);
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while (t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        preOrder(root);
+    }
+}
