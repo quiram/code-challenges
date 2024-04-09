070e8504fa2a95a104fc3b7f1cc10882ceaa86a4
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-04-09 16:15:03 -0700

    improve "Two Sum II - Input Array Is Sorted" to O(n)

--- src/main/java/com/github/quiram/challenges/medium/twosum_2/Solution.java
+++ src/main/java/com/github/quiram/challenges/medium/twosum_2/Solution.java
@@ -1,18 +1,22 @@
 package com.github.quiram.challenges.medium.twosum_2;
 
-import java.util.Arrays;
-
 /**
  * <a href="https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/">167. Two Sum II - Input Array Is Sorted</a>
  */
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            int j = Arrays.binarySearch(numbers, i + 1, numbers.length, target - numbers[i]);
-            if (j > -1) {
-                return new int[]{i + 1, j + 1};
+        int ini = 0, end = numbers.length - 1;
+        int sum;
+        do {
+            sum = numbers[ini] + numbers[end];
+            if (sum > target) {
+                end--;
+            } else if (sum < target) {
+                ini++;
             }
         }
-        throw new RuntimeException("solution should be guaranteed");
+        while (sum != target);
+
+        return new int[]{ini + 1, end + 1};
     }
 }
