9f5b1e8afae0cb6d8d9d3f297512733c39244518
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2024-04-11 14:42:17 -0700

    accelerate "953. Verifying an Alien Dictionary"
    Using `String.indexOf()` is, in theory, less performant than doing a looking in a hash table, since `indexOf()` does a linear search on the `String`. However, it seems that for this specific dataset, the cost of building the hash table does not compensate for the faster look-up in the `String`.

--- src/main/java/com/github/quiram/challenges/easy/aliendictionary/Solution.java
+++ src/main/java/com/github/quiram/challenges/easy/aliendictionary/Solution.java
@@ -1,19 +1,10 @@
 package com.github.quiram.challenges.easy.aliendictionary;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * <a href="https://leetcode.com/problems/verifying-an-alien-dictionary/">953. Verifying an Alien Dictionary</a>
  */
 class Solution {
     public boolean isAlienSorted(String[] words, String order) {
-        Map<Character, Integer> indices = new HashMap<>();
-        for (int i = 0; i < order.length(); i++) {
-            indices.put(order.charAt(i), i);
-        }
-
-
         for (int i = 0; i < words.length - 1; i++) {
             String thisWord = words[i];
             String nextWord = words[i + 1];
@@ -21,8 +12,8 @@ class Solution {
             int j = 0;
             boolean exit = false;
             while (!exit && j < thisWord.length() && j < nextWord.length()) {
-                int thisIndex = indices.get(thisWord.charAt(j));
-                int nextIndex = indices.get(nextWord.charAt(j));
+                int thisIndex = order.indexOf(thisWord.charAt(j));
+                int nextIndex = order.indexOf(nextWord.charAt(j));
 
                 if (thisIndex > nextIndex) {
                     return false;
