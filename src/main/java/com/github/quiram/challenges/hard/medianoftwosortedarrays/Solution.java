a9ad3a91f19982912954ed6416395ba61c4cd750
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2026-01-28 16:55:46 -0800

    solve with a loop instead of tail-recursion

--- src/main/java/com/github/quiram/challenges/hard/medianoftwosortedarrays/Solution.java
+++ src/main/java/com/github/quiram/challenges/hard/medianoftwosortedarrays/Solution.java
@@ -4,10 +4,10 @@ class Solution {
     public double findMedianSortedArrays(int[] a, int[] b) {
         final int n = a.length;
         final int m = b.length;
-        double k_th = solve(a, b, 0, n - 1, 0, m - 1, (n + m - 1) / 2);
+        double k_th = solve(a, b, n - 1, m - 1, (n + m - 1) / 2);
 
         if ((n + m) % 2 == 0) {
-            final double k1_th = solve(a, b, 0, n - 1, 0, m - 1, (n + m) / 2);
+            final double k1_th = solve(a, b, n - 1, m - 1, (n + m) / 2);
             return (k_th + k1_th) / 2;
         } else {
             return k_th;
@@ -15,36 +15,40 @@ class Solution {
 
     }
 
-    private double solve(int[] a, int[] b, int a_start, int a_end, int b_start, int b_end, int k) {
-        if (a_start > a_end) {
-            return b[k - a_start];
-        } else if (b_start > b_end) {
-            return a[k - b_start];
+    private double solve(int[] a, int[] b, int a_end, int b_end, int k) {
+        int a_start = 0;
+        int b_start = 0;
+        while (a_start <= a_end && b_start <= b_end) {
+            int a_mid = (a_end + a_start) / 2;
+            int b_mid = (b_end + b_start) / 2;
+            int a_median = a[a_mid];
+            int b_median = b[b_mid];
+            int threshold = a_mid + b_mid + 1;
+
+            if (a_median <= b_median) {
+                if (k >= threshold) {
+                    // discard lower half of A
+                    a_start = a_mid + 1;
+                } else {
+                    // discard upper half of B
+                    b_end = b_mid - 1;
+                }
+            } else {
+                // same, but inverting A and B
+                if (k >= threshold) {
+                    // discard lower half of B
+                    b_start = b_mid + 1;
+                } else {
+                    // discard upper half of A
+                    a_end = a_mid - 1;
+                }
+            }
         }
 
-        int a_mid = (a_end + a_start) / 2;
-        int b_mid = (b_end + b_start) / 2;
-        int a_median = a[a_mid];
-        int b_median = b[b_mid];
-        int threshold = a_mid + b_mid + 1;
-
-        if (a_median <= b_median) {
-            if (k >= threshold) {
-                // discard lower half of A
-                return solve(a, b, a_mid + 1, a_end, b_start, b_end, k);
-            } else {
-                // discard upper half of B
-                return solve(a, b, a_start, a_end, b_start, b_mid - 1, k);
-            }
+        if (a_start > a_end) {
+            return b[k - a_start];
         } else {
-            // same, but inverting A and B
-            if (k >= threshold) {
-                // discard lower half of B
-                return solve(a, b, a_start, a_end, b_mid + 1, b_end, k);
-            } else {
-                // discard upper half of A
-                return solve(a, b, a_start, a_mid - 1, b_start, b_end, k);
-            }
+            return a[k - b_start];
         }
     }
 }
