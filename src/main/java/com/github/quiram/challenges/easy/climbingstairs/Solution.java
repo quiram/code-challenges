a31999bdd116a7d87dadbb5a8dbf2131cb724331
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2026-01-29 09:52:27 -0800

    turn the solution into O(1) by using the Fibonacci simplified formula

--- src/main/java/com/github/quiram/challenges/easy/climbingstairs/Solution.java
+++ src/main/java/com/github/quiram/challenges/easy/climbingstairs/Solution.java
@@ -4,32 +4,14 @@ package com.github.quiram.challenges.easy.climbingstairs;
  * <a href="https://leetcode.com/problems/climbing-stairs/">70. Climbing Stairs</a>
  */
 class Solution {
-    int[] cache;
-
     public int climbStairs(int n) {
-        cache = new int[n + 1];
-        for (int i = 0; i <= n; i++)
-            cache[i] = -1;
-        return solve(n);
+        /*
+        This is essentially a Fibonacci sequence, for which a formula exists.
+        The formula is zero-indexed so we need to increase n to make it work.
+         */
+        n++;
+        final double sqrt_5 = Math.sqrt(5);
+        final double phi = (1 + sqrt_5) / 2;
+        return (int) Math.round(Math.pow(phi, n) / sqrt_5);
     }
-
-    private int solve(int n) {
-        if (cache[n] != -1)
-            return cache[n];
-
-        final int result;
-        if (n == 0)
-            result = 0;
-        else if (n == 1)
-            result = 1;
-        else if (n == 2)
-            result = 2;
-        else
-            result = solve(n - 1) + solve(n - 2);
-
-        cache[n] = result;
-        return result;
-    }
-
-
 }
