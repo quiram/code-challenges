c542962b661688c50773b66b60128b127f0f2e8c
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2026-02-04 14:48:21 -0800

    reduce complexity to O(n*m) by re-implementing as build-forward instead of look-back

--- src/main/java/com/github/quiram/challenges/medium/longestcommonsubsequence/Solution.java
+++ src/main/java/com/github/quiram/challenges/medium/longestcommonsubsequence/Solution.java
@@ -1,35 +1,21 @@
 package com.github.quiram.challenges.medium.longestcommonsubsequence;
 
-import java.util.Arrays;
-
 /**
  * <a href="https://leetcode.com/problems/longest-common-subsequence/description/">1143. Longest Common Subsequence</a>
  */
 class Solution {
-    private int[][] memo;
-
     public int longestCommonSubsequence(String text1, String text2) {
-        memo = new int[text1.length()][text2.length()];
-        for (int[] array : memo)
-            Arrays.fill(array, -1);
+        int[][] results = new int[text1.length() + 1][text2.length() + 1];
 
-        return solve(text1, text2, text1.length(), text2.length());
-    }
+        for (int i = 1; i <= text1.length(); i++) {
+            for (int j = 1; j <= text2.length(); j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1))
+                    results[i][j] = 1 + results[i - 1][j - 1];
+                else
+                    results[i][j] = Math.max(results[i - 1][j], results[i][j - 1]);
+            }
+        }
 
-    private int solve(String text1, String text2, int i1, int i2) {
-        if (i1 == 0 || i2 == 0)
-            return 0;
-
-        if (memo[i1 - 1][i2 - 1] > -1)
-            return memo[i1 - 1][i2 - 1];
-
-        final int result;
-        if (text1.charAt(i1 - 1) == text2.charAt(i2 - 1))
-            result = 1 + solve(text1, text2, i1 - 1, i2 - 1);
-        else
-            result = Math.max(solve(text1, text2, i1 - 1, i2), solve(text1, text2, i1, i2 - 1));
-
-        memo[i1 - 1][i2 - 1] = result;
-        return result;
+        return results[text1.length()][text2.length()];
     }
 }
