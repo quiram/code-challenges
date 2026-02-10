5f7c0669725d3e2ad9df925353d470df3b8f918f
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2026-02-10 09:33:23 -0800

    improve performance by removing if statements in the inner loop

--- src/main/java/com/github/quiram/challenges/medium/uniquepaths/Solution.java
+++ src/main/java/com/github/quiram/challenges/medium/uniquepaths/Solution.java
@@ -8,13 +8,16 @@ class Solution {
         int[][] results = new int[m][n];
         results[m - 1][n - 1] = 1;
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (j < n - 1)
-                    results[i][j] += results[i][j + 1];
+        for (int i = 0; i < m; i++)
+            results[i][n - 1] = 1;
 
-                if (i < m - 1)
-                    results[i][j] += results[i + 1][j];
+        for (int j = 0; j < n; j++)
+            results[m - 1][j] = 1;
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                results[i][j] += results[i][j + 1];
+                results[i][j] += results[i + 1][j];
             }
         }
 
