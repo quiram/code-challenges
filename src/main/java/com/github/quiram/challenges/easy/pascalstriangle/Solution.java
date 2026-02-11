86488825ae411a4ca5bca7a15241dc0d81649ca3
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2026-02-11 10:21:12 -0800

    implement Pascal's Triangle with complexity O(n^2)

---
+++ src/main/java/com/github/quiram/challenges/easy/pascalstriangle/Solution.java
@@ -0,0 +1,32 @@
+package com.github.quiram.challenges.easy.pascalstriangle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <a href="https://leetcode.com/problems/pascals-triangle/description/">118. Pascal's Triangle</a>
+ */
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (numRows >= 1)
+            result.add(List.of(1));
+
+        if (numRows >= 2)
+            result.add(List.of(1, 1));
+
+        for (int i = 2; i < numRows; i++) {
+            final List<Integer> seed = result.get(i - 1);
+            List<Integer> newRow = new ArrayList<>();
+            newRow.add(1);
+            for (int j = 1; j < seed.size(); j++) {
+                newRow.add(seed.get(j) + seed.get(j - 1));
+            }
+            newRow.add(1);
+            result.add(newRow);
+        }
+
+        return result;
+    }
+}
