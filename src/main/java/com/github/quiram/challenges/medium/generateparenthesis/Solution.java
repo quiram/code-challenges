a52082e811f7a19654e732c5019f42ed230d0745
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2026-02-12 15:20:23 -0800

    re-implement Generate Parenthesis as a Catalan Sequence
    This is less intuitive and produces the same time complexity, but at least it doesn't use recursion.

--- src/main/java/com/github/quiram/challenges/medium/generateparenthesis/Solution.java
+++ src/main/java/com/github/quiram/challenges/medium/generateparenthesis/Solution.java
@@ -5,25 +5,29 @@ import java.util.List;
 
 /**
  * <a href="https://leetcode.com/problems/generate-parentheses/description/">22. Generate Parenthesis</a>
+ * <p>
+ * This is a Catalan sequence that can be defined as follows
+ * f(0) = ""
+ * f(n > 0) = Union( "(" + f(i) + ")" + f(n-i-1) ) for i = 0...n-1
  */
 class Solution {
+
     public List<String> generateParenthesis(int n) {
-        return solve(new StringBuilder(), 0, n);
-    }
+        List<StringBuilder>[] result = new List[n + 1];
+        result[0] = List.of(new StringBuilder());
 
-    public List<String> solve(StringBuilder currentString, int open, int toOpen) {
-        if (open == 0 && toOpen == 0)
-            return List.of(currentString.toString());
-
-        List<String> results = new ArrayList<>();
-
-        if (open > 0) {
-            results.addAll(solve(new StringBuilder(currentString).append(')'), open - 1, toOpen));
+        for (int i = 1; i < result.length; i++) {
+            result[i] = new ArrayList<>();
+            for (int j = 0; j < i; j++) {
+                for (StringBuilder sb1 : result[j])
+                    for (StringBuilder sb2 : result[i - j - 1])
+                        result[i].add(new StringBuilder().append('(').append(sb1).append(')').append(sb2));
+            }
         }
 
-        if (toOpen > 0) {
-            results.addAll(solve(new StringBuilder(currentString).append('('), open + 1, toOpen - 1));
-        }
+        final List<String> results = new ArrayList<>();
+        for (StringBuilder sb : result[n])
+            results.add(sb.toString());
 
         return results;
     }
